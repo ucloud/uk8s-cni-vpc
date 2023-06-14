6f1041e5becad8670125a688cdf64cae062e237a
wenqian <lazycat7706@gmail.com>
2023-06-14 14:14:02 +0800

    feat(cnivpc): take snapshot after adding route fails (#20)

---
+++ pkg/snapshot/snapshot_test.go
@@ -0,0 +1,20 @@
+package snapshot
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestTakeSnapshot(t *testing.T) {
+	s := New("test-snapshot")
+
+	s.SetError(errors.New("test error"))
+	s.SetDesc("This is a test snapshot")
+
+	s.Add("ip", "addr")
+	s.Add("ip", "link")
+	s.Add("ip", "route")
+	s.Add("ip", "netns", "list-id")
+
+	s.Save()
+}
